868636a542c15b578014a81d67dd5cb050a8df3f
Josue Mendez <josuemendez@users.noreply.github.com>
2021-10-08 02:34:54 -0500

    update

--- inc/slider.php
+++ inc/slider.php
@@ -8,14 +8,14 @@
         <div class="slider-item item-one">
           <div class="container">
             <div class="banner-content">
-            <span class="sub-title">Regata</span>
+           <!-- <span class="sub-title">Regata</span>
                         <h1>
                             COPA BICENTENARIO
                         </h1>
                         <p>
                             "200 Años de la armada de México"
                         </p>
-              <a href="copabicentenario2021/index.php" class="btn-primary">Registrarme</a>
+              <a href="copabicentenario2021/index.php" class="btn-primary">Registrarme</a>-->
             </div>
 
 
@@ -25,14 +25,14 @@
         <div class="slider-item item-two">
           <div class="container">
           <div class="banner-content">
-            <span class="sub-title">Inscripción</span>
+           <!-- <span class="sub-title">Inscripción</span>
                         <h1>
                             al Club 2020
                         </h1>
                         <p>
                             Para todas las embarcaciones que quieran navegar
                         </p>
-              <a href="http://www.reactivacion.yucatan.gob.mx" class="btn-primary">Registro</a>
+              <a href="http://www.reactivacion.yucatan.gob.mx" class="btn-primary">Registro</a>-->
             </div>
           </div>
         </div>
